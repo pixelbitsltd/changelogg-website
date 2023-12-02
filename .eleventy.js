746d25aa6e1817f21ed31b686aa958be5b797a19
Dave Irvine <dave@pixelbits.co.uk>
2023-12-02 21:22:44 +0000

    Implement A/B on pricing

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,23 @@
 const { EleventyI18nPlugin, EleventyHtmlBasePlugin, EleventyRenderPlugin } = require('@11ty/eleventy');
+const rollupPlugin = require('eleventy-plugin-rollup');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
 
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addPassthroughCopy({ "src/assets/js/*": "js" });
   eleventyConfig.addPassthroughCopy({ "src/assets/img/*": "img" });
   eleventyConfig.addPassthroughCopy({
     "node_modules/preline/dist/preline.js": "js/preline.js"
+  });
+  eleventyConfig.addWatchTarget('src/assets/js/');
+
+  eleventyConfig.addPlugin(rollupPlugin, {
+    rollupOptions: {
+      output: {
+        format: 'es',
+        dir: 'dist/js',
+      },
+      plugins: [nodeResolve()]
+    },
+    useAbsoluteScriptPaths: true,
   });
 
   return {
--- src/assets/js/pricing.js
+++ src/assets/js/pricing.js
@@ -1,7 +1,11 @@
-function pushPricingToHistory(e) {
+function selectPricing(e) {
   const { target } = e;
 
   const pricing = target.dataset['pricing'];
+
+  posthog.capture('user_selected_pricing', {
+    pricing,
+  });
 
   const searchParams = new URLSearchParams(window.location.search);
   searchParams.set("pricing", pricing);
@@ -10,10 +14,33 @@ function pushPricingToHistory(e) {
   history.pushState(null, '', newRelativePathQuery);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function assignDynamicPricing(pricing) {
+  for (const [category, { monthly, annual }] of Object.entries(pricing)) {
+    const categoryId = `${category}-pricing`;
+    const monthlyElement = document.querySelector(`#${categoryId} h2.monthly-price`);
+    const annualElement = document.querySelector(`#${categoryId} h2.annual-price`);
+
+    monthlyElement.textContent = `$${monthly}`;
+    annualElement.textContent = `$${annual}`;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
   const pricingButtons = Array.from(document.querySelectorAll('.pricing-button'));
 
   for (const pricingButton of pricingButtons) {
-    pricingButton.addEventListener('click', pushPricingToHistory);
+    pricingButton.addEventListener('click', selectPricing);
   }
+
+  posthog.onFeatureFlags(function () {
+    if (posthog.getFeatureFlag('pricing-ab') === 'test') {
+      const { starter, plus, pro } = posthog.getFeatureFlagPayload('pricing-ab');
+
+      assignDynamicPricing({
+        starter,
+        plus,
+        pro
+      });
+    }
+  });
 });
